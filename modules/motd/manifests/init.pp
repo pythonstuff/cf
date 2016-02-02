3dfe24efec811a4cb63241b849ad4c24182b4157
mark cater <mark.cater@me.com>
2016-02-02 14:22:56 +0000

    phase5

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,7 +1,7 @@
 class motd {
 
-        file { 'qwe':
-            name    => '/tmp/qwe',
+        file { 'Auto_Created_By_Puppet':
+            name    => '/tmp/Auto_Created_By_Puppet',
             mode    => '0664',
             owner   => 'root',
             group   => 'root',
