9d9d969314f53707e585f9b9565fab8958d1b8ec
mark cater <mark.cater@me.com>
2016-02-02 11:55:51 +0000

    updated motd

--- motd/manifests/init.pp
+++ motd/manifests/init.pp
@@ -1,14 +1,12 @@
-# puppet module test
-class motd
-{
-  file
-  {
-    '/etc/motd':
-      ensure  => file,
-      mode    => 644,
-      content => "Welcome to ${::hostname} - ${::fqdn} - ${::operatingsystem} ${::operatingsystemrelease}
-This node is under the control of Puppet ${::puppetversion}
-TEST
-";
-  }
-}
+class motd {
+
+        $ascii = generate('/bin/sh', '-c', "/usr/bin/figlet -c -w 60 ${hostname}")
+
+        file { 'motd':
+            name    => '/etc/motd',
+            mode    => '0664',
+            owner   => 'root',
+            group   => 'root',
+            content => template('motd/motd.erb')
+        }
+    }
